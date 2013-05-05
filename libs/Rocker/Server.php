362838ab1d4473035a9bdda187dc413efea51a89
Victor Jonsson <kontakt@victorjonsson.se>
2013-05-05 10:22:21 +0200

    changed version number

--- libs/Rocker/Server.php
+++ libs/Rocker/Server.php
@@ -21,7 +21,7 @@ class Server extends \Slim\Slim  {
     /**
      * @const Current version of Rocker
      */
-    const VERSION = '1.1.1';
+    const VERSION = '1.1.0';
 
     /**
      * @var array
