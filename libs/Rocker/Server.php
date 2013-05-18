36f89b4f661de334cfa0e8c24d2f2ff026a9f75b
Victor Jonsson <kontakt@victorjonsson.se>
2013-05-18 09:25:38 +0200

    packagist here we go

--- libs/Rocker/Server.php
+++ libs/Rocker/Server.php
@@ -21,7 +21,7 @@ class Server extends \Slim\Slim  {
     /**
      * @const Current version of Rocker
      */
-    const VERSION = '1.1.4';
+    const VERSION = '1.1.5';
 
     /**
      * @var array
