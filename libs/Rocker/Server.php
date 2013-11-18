fcd384d03c77283ac74d76bd0a83823cb2aa44bb
Victor Jonsson <kontakt@victorjonsson.se>
2013-11-18 20:50:44 +0100

    new version on the way..."

--- libs/Rocker/Server.php
+++ libs/Rocker/Server.php
@@ -21,7 +21,7 @@ class Server extends \Slim\Slim  {
     /**
      * @const Current version of Rocker
      */
-    const VERSION = '1.1.11';
+    const VERSION = '1.1.12';
 
     /**
      * @var array
