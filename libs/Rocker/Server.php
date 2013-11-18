50852b9e8a7e8887cd68a715c6a5e30dab7bb514
Victor Jonsson <kontakt@victorjonsson.se>
2013-11-18 20:59:08 +0100

    .htaccess now added when installed

--- libs/Rocker/Server.php
+++ libs/Rocker/Server.php
@@ -21,7 +21,7 @@ class Server extends \Slim\Slim  {
     /**
      * @const Current version of Rocker
      */
-    const VERSION = '1.2.0';
+    const VERSION = '1.2.1';
 
     /**
      * @var array
